b373ac99906f0a000d7327d028805626a5aa044b
rchan3 <r.achiechan@gmail.com>
2022-04-13 16:22:43 -0400

    add logged in and not logged in ui + error message for wrong login details

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -4,46 +4,69 @@ import "./Login.css";
 const Login = (props) => {
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
-  const [checkUser, setCheckUser] = useState();
+
+  const [isLoggedIn, setIsLoggedIn] = useState(undefined);
 
   const getUser = async (user, pass) => {
     const res = await fetch(
       `http://localhost:3001/login?username=${user}&pw=${pass}`,
       { method: "GET" }
     );
-    console.log(res);
+
     const jsonData = await res.json();
-    setCheckUser(jsonData);
+    console.log(jsonData.loggedIn);
+    setIsLoggedIn(jsonData.loggedIn);
   };
 
-  const handleSubmit = (i) => {
+  const handleLogin = (i) => {
     i.preventDefault();
     getUser(username, password);
   };
 
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          username:
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </label>
-        <label>
-          password:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-        <input type="submit" value="submit" />
-      </form>
-    </div>
-  );
+  const wrongUserOrPassword = () => {
+    if (isLoggedIn === false) {
+      return (
+        <div>
+          <p>the username or password is incorrect</p>
+        </div>
+      );
+    } else {
+      return <></>;
+    }
+  };
+
+  if (isLoggedIn === true) {
+    return (
+      <div>
+        <p>{`currently logged in as ${username}`}</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <form onSubmit={handleLogin}>
+          <label>
+            username:
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </label>
+          <label>
+            password:
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <input type="submit" value="submit" />
+        </form>
+        {wrongUserOrPassword()}
+      </div>
+    );
+  }
 };
 
 export default Login;
